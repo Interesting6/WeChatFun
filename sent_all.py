43251cd0d9184f803ea2df91774a2331f54cb3d1
Treamy <cym1106515697@outlook.com>
2018-02-16 12:46:17 +0800

    Update sent_all.py
    fix some bug.

--- sent_all.py
+++ sent_all.py
@@ -1,9 +1,9 @@
 #coding=utf8
-import itchat, time
+import itchat, time,requests,random
 
-itchat.auto_login()
+itchat.auto_login(hotReload=True)
 
-SINCERE_WISH = u'祝%s新年快乐！'
+SINCERE_WISH = u'新年快乐！'
 
 def getRandomGreeting():
   response = requests.get("http://www.xjihe.com/api/life/greetings?festival=新年&page=10", headers = {'apiKey':'sQS2ylErlfm9Ao2oNPqw6TqMYbJjbs4g'})
@@ -14,9 +14,14 @@ def getRandomGreeting():
 
 friendList = itchat.get_friends(update=True)[1:]
 
+print(len(friendList))
+
 count = 0
 for friend in friendList:
-    itchat.send( SINCERE_WISH % (friend['DisplayName']
-         or friend['NickName']), friend['UserName'])
-    time.sleep(1)
+	regreting = SINCERE_WISH + getRandomGreeting() + "--by ***your name***'s wechat robot. \nPS:心意在我的程序里，嘿嘿嘿"
+	regreting = "亲爱的%s: " % (friend['RemarkName'] or friend['NickName']) + regreting
+	itchat.send(regreting,friend["UserName"])
+	time.sleep(5)
+
+
 print("----end----")
